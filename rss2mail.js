f623a9a5a0703c3dfefcdc4ac1a80be994fc779f
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2024-03-08 10:59:21 +0200

    add --no-lock CLO

--- rss2mail.js
+++ rss2mail.js
@@ -3,7 +3,6 @@
 import {pipeline} from 'stream'
 import util from 'util'
 import fs from 'fs'
-import {fileURLToPath} from 'url'
 import path from 'path'
 import FeedParser from 'feedparser'
 import lockfile from 'proper-lockfile'
@@ -13,14 +12,14 @@ function errx(e) {
     console.error(`rss2mail error (${process.env.RSS2MAIL || process.pid}):`, e.message)
     process.exit(1)
 }
-let __dirname = path.dirname(fileURLToPath(import.meta.url))
+let __dirname = path.dirname(new URL('', import.meta.url).pathname)
 
 let args
 try {
     args = util.parseArgs({allowPositionals: true, options: {
         rnews: { type: 'boolean' }, help: { type: 'boolean', short: 'h' },
         history: { type: 'string' }, f: { type: 'string' },
-        o: { type: 'string' },
+        o: { type: 'string' }, 'no-lock': { type: 'boolean' }
     }})
 } catch (err) {
     errx(err)
@@ -35,12 +34,15 @@ let out = process.stdout
 let lock = Promise.resolve( () => {/* nop */})
 if (args.values.o) {
     out = fs.createWriteStream(args.values.o, {flags: 'a'})
-    lock = lockfile.lock(args.values.o, {
-        retries: { // living dangerously
-            retries: 0, forever: true, minTimeout: 10, maxTimeout: 100,
-            randomize: true
-        }
-    })
+    out.on('error', errx)
+    if (!args.values['no-lock']) {
+        lock = lockfile.lock(args.values.o, {
+            retries: { // living dangerously
+                retries: 0, forever: true, minTimeout: 10, maxTimeout: 100,
+                randomize: true
+            }
+        })
+    }
 }
 streams.push(out)
 
